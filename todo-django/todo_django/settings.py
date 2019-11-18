3315f203148345c50a55369b7d06bf692cd93067
Ssong10 <leesy1403@naver.com>
2019-11-18 15:03:48 +0900

    Django | setting CORS

--- todo-django/todo_django/settings.py
+++ todo-django/todo_django/settings.py
@@ -38,12 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # CORS - 반드시 CommonMiddleware 위에 추가
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +125,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'todos.User'
+
+CORS_ORIGIN_ALLOW_ALL = True # CORS 모두 추가
+
+CORS_ORIGIN_WHITELIST = [
+    # 추후에 배포시 vue에서만 요청 보낼 수 있도록 정의 !
+]
