cc7b087968609f1b5b96d5a1db03bd86df0ea31c
Ssong10 <leesy1403@naver.com>
2019-11-19 16:02:04 +0900

    Vue | Add todo Update/Delete

--- todo-vue/src/components/TodoList.vue
+++ todo-vue/src/components/TodoList.vue
@@ -2,14 +2,16 @@
   <div class="todo-list">
     <ul>
       <li @click="todo_click(todo)" v-for="todo in todos" :key="todo.id">
-        <del v-if="todo.is_completed">{{todo.title}}</del>
-        <strong v-else>{{todo.title}}</strong>
+        <input @change="todo_completed(todo)" type="checkbox" v-model="todo.is_completed">
+        {{todo.title}}
+        <button @click="todo_delete(todo)">X</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name:'TodoList',
     props :{
@@ -21,6 +23,45 @@ export default {
     methods : {
       todo_click(todo) {
         todo.is_completed = !todo.is_completed
+      },
+      todo_delete(todo) {
+        this.$session.start()
+        const token = this.$session.get('jwt')
+        const options = {
+          headers : {
+          Authorization : `JWT ${token}`
+          }
+        }
+        axios.delete(`http://127.0.0.1:8000/api/v1/todos/${todo.id}/`,options)
+        .then(response => {
+          console.log(response)
+          this.todos = this.todos.filter( el => {
+            return el !== todo
+          })
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      },
+      todo_completed(todo) {
+        console.log(todo)
+        this.$session.start()
+        const token = this.$session.get('jwt')
+        const options = {
+          headers : {
+          Authorization : `JWT ${token}`
+          }
+        }
+        const data = {
+          "id" : todo.id,
+          "title" : todo.title,
+          "user" : todo.user,
+          "is_completed" : todo.is_completed
+        }
+        axios.put(`http://127.0.0.1:8000/api/v1/todos/${todo.id}/`,data,options)
+        .then(response => {
+          console.log(response)
+        })
       }
     }
 }
